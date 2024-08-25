b6362e3f3ec879efaf49a0af5b395878149c02ac
Raja Farras Daffa <rajafarras63@gmail.com>
2024-08-26 00:01:45 +0700

    menggunakan axios

---
+++ src/components/Pagination.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+	page: Number,
+	totalPages: Number,
+});
+
+const emit = defineEmits(["change-page"]);
+
+function changePage(newPage) {
+	emit("change-page", newPage)
+}
+</script>
+
+<template>
+    <main>
+        <button class="pagination-button" id="prevPage" @click="changePage(page - 1)" v-show="page > 1">&lt;</button>
+		<span>Page {{ page }}</span>
+		<button class="pagination-button" id="nextPage" @click="changePage(page + 1)" v-show="page < totalPages">&gt;</button>
+    </main>
+</template>
+
+<style scoped>
+.pagination-button {
+	background-color: #fff;
+	color: #ff0095;
+	padding: 8px 12px;
+	border: none;
+	border-radius: 50%;
+	cursor: pointer;
+	font-size: 16px;
+	margin: 0 5px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<script setup>
+import ProductCard from '@/components/ProductCard.vue';
+import Pagination from '@/components/Pagination.vue';
+
+import { ref, watch } from 'vue';
+import axios from 'axios';
+
+const products = ref([]);
+const page = ref(1);
+const limit = ref(8);
+
+//Suspense Componen
+products.value = await axios.get(`http://localhost:3000/products?_page=${page.value}&_per_page=${limit.value}`).then((res) => res.data);
+
+watch(page, async () => {
+	products.value = await axios.get(`http://localhost:3000/products?_page=${page.value}&_per_page=${limit.value}`).then((res) => res.data);
+})
+console.log(products.value);
+
+function changePage(newPage) {
+	if (newPage < 1) return;
+	if (newPage > products.value.pages) return;
+	page.value = newPage
+}
+
+//fetch data menggunakan axios dan async component
+// async function getProducts() {
+// 	const response = await axios.get("http://localhost:3000/products");
+// 	products.value = response.data;
+// 	console.log(response.data);
+// }
+// getProducts()
+
+</script>
+
+<template>
+	<main>
+		<div class="product-grid">
+			<ProductCard v-for="(product, index) in products.data" :key="index" :product="product" />
+		</div>
+
+		<div class="pagination">
+			<Pagination :page="page" :totalPages="products.pages" @change-page="changePage" />
+		</div>
+	</main>
+</template>
+
+<style scoped>
+.product-grid {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	gap: 20px;
+	width: 80%;
+	margin: 0 auto;
+}
+
+
+
+.pagination {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 20px;
+}
+
+
+</style>
